a3da8a1d08b158c550cb8deb9bddbe52ef591716
liuyu <queeniiee02@gmail.com>
2023-03-26 11:50:12 +0800

    G is about 23

--- hw1/hw1.r
+++ hw1/hw1.r
@@ -12,14 +12,22 @@ nll.condition=function(par,y) {
 
 # same as modeling book
 nll.1=function(par10,y20) {
+    model=1:10
     result=0.0
+    par=1:2
+    params=NULL
+    
+    print("params 1")
     for (i in 0:4) {
         start2=i*2+1
         end2=start2+1
         start4=i*4+1
         end4=start4+3
-        result=result+nll.condition(par10[start2:end2], y20[start4:end4])
+        model=optim(par=par10[start2:end2], nll.condition, y=y20[start4:end4])
+        result=result+model$value
+        params=c(params,model$par)
     }
+    print(params)
     return(result)
 }
 
@@ -37,14 +45,15 @@ nll.2=function(par10,y20) {
 }
 
 data=c(404,96,301,199,348,152,235,265,287,213,183,317,251,249,102,398,148,352,20,480)
-par=1:10
+par10=1:10
 for (i in 1:10) {
-    par[i]=0.5 # assume all d and g are 0.5
+    par10[i]=0.5 # assume all d and g are 0.5
 }
-mod1=optim(par,nll.1,y20=data)
-par_m1 = mod1$par
+mod1=nll.1(par10, y20=data)
+#mod1=optim(par,nll.1,y20=data)
+par_m1 = mod1
 print("params 1") # print the value of model 1
-print(par_m1)
+# print(par_m1)
 par=1:6
 for (i in 1:6) {
     par[i]=0.5 # assume all d and g are 0.5
@@ -54,6 +63,6 @@ par_m2 = mod2$par
 print("params 2")
 print(par_m2) # print the value of model 2
 chi_square=qchisq(p=0.05, df=4, lower.tail=FALSE)
-G_square=-2*(mod2$value-mod1$value) # use simplyfied G ratio test
+G_square=2*(mod2$value-mod1) # use simplyfied G ratio test
 print(paste("chi_square:", chi_square))
 print(paste("G_square:", G_square))
